017f3826e12a0dc6c70c0147eea45d880ecb0575
ecp-alex <alex.mackenzie@englishcollege.cz>
2019-12-09 21:52:35 +0100

    01 | add routing | navbar + items + item routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
+import Home from "./pages/Home";
+import Items from "./pages/Items";
+import Item from "./pages/Item";
+import Error from "./pages/Error";
+
+import { Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/items" component={Items} />
+        <Route exact path="/items/:slug" component={Item} />
+        <Route component={Error} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Item.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Item extends Component {
+  render() {
+    return <div>Hello from Item page</div>;
+  }
+}
