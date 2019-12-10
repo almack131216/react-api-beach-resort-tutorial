3de5c2deeed0d7008c421925f688a903100075dd
ecp-alex <alex.mackenzie@englishcollege.cz>
2019-12-10 06:25:41 +0100

    05 | Context + Loading + Featured Items + Item

--- src/Context.js
+++ src/Context.js
@@ -1,11 +1,37 @@
 import React, { Component } from "react";
+import Data from "./data";
 const ItemContext = React.createContext();
 //
 export default class ItemProvider extends Component {
   state = {
-    greeting: "hey",
-    name: "Alex"
+    items: [],
+    sortedItems: [],
+    featuredItems: [],
+    loading: true
   };
+  // getData
+  componentDidMount() {
+    let items = this.formatData(Data);
+    console.log("[Context.js] items...", items);
+    let featuredItems = items.filter(item => item.featured === true);
+    this.setState({
+      items,
+      featuredItems,
+      sortedItems: items,
+      loading: false
+    });
+  }
+
+  formatData(Data) {
+    let tempItems = Data.map(dataItem => {
+      let id = dataItem.sys.id;
+      let images = dataItem.fields.images.map(image => image.fields.file.url);
+      let item = { ...dataItem.fields, images, id };
+      return item;
+    });
+    return tempItems;
+  }
+
   render() {
     return (
       <ItemContext.Provider value={{ ...this.state }}>
--- src/components/FeaturedItems.js
+++ src/components/FeaturedItems.js
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
 import { ItemContext } from "../Context";
+import Loading from "./Loading";
+import Item from "./Item";
+import Title from "./Title";
 
 export default class FeaturedItems extends Component {
   static contextType = ItemContext;
 
   render() {
-    const { name, greeting } = this.context;
+    let { loading, featuredItems: items } = this.context;
+    console.log("[FeaturedItems.js] items...", items);
+    items = items.map(item => {
+      return <Item key={item.id} item={item} />;
+    });
 
     return (
-      <div>
-        {greeting} {name} from featured items
-      </div>
+      <section className="featured-rooms">
+        <Title title="Featured Items" />
+        <div className="featured-rooms-center">
+          {loading ? <Loading /> : items}
+        </div>
+      </section>
     );
   }
 }
